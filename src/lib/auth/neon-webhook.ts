7ff083d3166ca5d64bc461bec45fd3277a1f6d0a
synapselab-ia <synapselab.ia@gmail.com>
2026-09-04 09:22:27 -0300

    fix(auth): use Node-compatible JWK typing

--- src/lib/auth/neon-webhook.ts
+++ src/lib/auth/neon-webhook.ts
@@ -10,7 +10,13 @@ const UUID_PATTERN =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const ALLOWED_EVENT_TYPES = new Set(["user.before_create", "user.created"]);
 
-type JsonWebKeyWithKid = JsonWebKey & { kid?: string; alg?: string };
+type JsonWebKeyWithKid = {
+  [key: string]: string | string[] | undefined;
+  kid?: string;
+  alg?: string;
+  kty?: string;
+  crv?: string;
+};
 type JwksDocument = { keys?: JsonWebKeyWithKid[] };
 
 type CacheEntry = {
